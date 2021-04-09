8971c71d0b11d066ca81c3c9c2b4365b68abacb3
emsoaldin <emsoaldin@gmail.com>
2021-04-09 12:39:16 +0200

    Fixed grammar mistakes

--- src/components/About/AboutMe.js
+++ src/components/About/AboutMe.js
@@ -4,17 +4,18 @@ const AboutMe = () => {
   return (
     <div>
       <p className='mb-3'>
-        Hello! I'm Aldin Emšo, BSc. ing. of CIT, currently working as Full stack
-        developer at Ministry of Programming. <br></br>I have serious passion
-        for web development, especially love working on back-end related stuff.
+        Hello! I'm Aldin Emšo, BSc. ing. of CIT, currently working as a
+        Full-stack developer at the Ministry of Programming. <br></br>I have a
+        serious passion for web development, especially love working on back-end
+        related stuff.
       </p>
       <p mb-3>
-        I consider my self as a good problem solver and quality thinker. I'm a
+        I consider myself as a good problem solver and quality thinker. I'm a
         person who loves spending time working <br></br> and discovering new
         stuff. Big fan of football and also passionate about cars.
       </p>
       <p>
-        I really enjoy spending time and working with positive people on
+        I enjoy spending time developing and working with positive people on
         ambitious projects.
       </p>
     </div>
--- src/components/About/Skills.js
+++ src/components/About/Skills.js
@@ -4,24 +4,24 @@ const Skills = () => {
   return (
     <div>
       <p className='mb-3'>
-        At the Faculty I've managed to accuire lots of IT skills. My first
-        passion was building desktop computer,<br></br>
-        where love for the IT is born. During my education, I had a chance to
-        accuire a networking and web developing skills.<br></br>
-        I've started with basics of HTML and CSS, and later on encouturing the
+        At the Faculty, I've managed to acquire lots of IT skills. My first
+        passion was building a desktop computers,<br></br>
+        where my love for IT is born. During my education, I had a chance to
+        acquire networking and web developing skills.<br></br>
+        I've started with the basics of HTML and CSS, and later on encountering
         PHP and Javascript.<br></br>
       </p>
       <p>
-        Right now, I'm mainly working with ReactJS for front-end developing, and
-        NodeJS for back-end developing.<br></br>
+        Right now, I'm mainly working with ReactJS for front-end development and
+        NodeJS for back-end development.<br></br>
         Worked on many projects at the Faculty and outside of it.{" "}
         {/*If you are
         interested, check out few of them in 'My projects' section.*/}
       </p>
       <p>
-        Also I would like to point out that I really like working with other
-        people.<br></br>I concider my self as a communicative person with
-        ability of easy adopting to new enviroments.
+        Also I would like to point out that I like working with other people.
+        <br></br>I consider myself a communicative person with the ability to
+        easily adapt to new environments.
       </p>
     </div>
   );
