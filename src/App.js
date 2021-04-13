f0cb3ce279163d933ab7ce4b91fd185649f8e08f
emsoaldin <emsoaldin@gmail.com>
2021-04-13 16:11:38 +0200

    Change

--- src/App.js
+++ src/App.js
@@ -8,25 +8,24 @@ const App = () => {
   return (
     <Router>
       <div>
-        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-          <ul className='navbar-nav mr-auto'>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
             <li>
-              <Link to={"/"} className='nav-link'>
+              <Link to={"/"} className="nav-link">
                 Home
               </Link>
             </li>
             <li>
-              <Link to={"/contact"} className='nav-link'>
+              <Link to={"/contact"} className="nav-link">
                 Contact
               </Link>
             </li>
           </ul>
-          
         </nav>
         <hr />
         <Switch>
-          <Route exact path='/' component={About} />
-          <Route path='/contact' component={Contact} />
+          <Route exact path="/" component={About} />
+          <Route path="/contact" component={Contact} />
         </Switch>
       </div>
     </Router>
